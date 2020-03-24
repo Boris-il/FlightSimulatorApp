6225a9dd9099635336312a592369bbc5090820ac
Boris Ilchenkov <boris.ilchenkov@gmail.com>
2020-03-24 14:50:39 +0200

    Created new project

---
+++ FlightSimulator/IFlyModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlightSimulator;
+
+namespace FlightSimulator
+{
+    interface IFlyModel
+    {
+        void connect(string ip, int port);
+    }
+
+    class MyFlyModel : IFlyModel
+    {
+        IClient telnetClient;
+        volatile Boolean stop;
+
+        public void connect(string ip, int port)
+        {
+            // todo
+        }
+    }
+}
