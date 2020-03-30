35476fb62918a271030f421a70fd493e9a6bccf2
Boris Ilchenkov <boris.ilchenkov@gmail.com>
2020-03-30 16:03:57 +0300

    Updated Joystick + Added JoystickViewModel class

--- FlightSimulator/Joystick.xaml.cs
+++ FlightSimulator/Joystick.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using FlightSimulator;
 
 namespace FlightSimulator
 {
@@ -23,34 +12,33 @@ namespace FlightSimulator
     {
         public Joystick()
         {
-          InitializeComponent();
+            InitializeComponent();
         }
 
         Point start = new Point();
         Point current = new Point();
-        private double x1, y1;
         bool mousePressed;
 
 
         private void Knob_MouseUp(object sender, MouseButtonEventArgs e)
         {
             this.mousePressed = false;
-            centerKnob_Completed(sender, e); //check
+            centerKnob_Completed(sender, e);
         }
 
         private void centerKnob_Completed(object sender, EventArgs e)
         {
-            Canvas.SetLeft(Knob, 125);
-            Canvas.SetTop(Knob, 125);
+            knobPosition.X = 0;
+            knobPosition.Y = 0;
         }
 
         private void Knob_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            if(!mousePressed)
+            if (!mousePressed)
             {
                 // set initial mouse coordinates
-                this.start.X = e.GetPosition(sender as Canvas).X;
-                this.start.Y = e.GetPosition(sender as Canvas).Y;
+                this.start.X = e.GetPosition(this).X;
+                this.start.Y = e.GetPosition(this).Y;
                 Console.WriteLine("start X: {0}, start Y: {1}", this.start.X, this.start.Y);
                 this.mousePressed = true;
             }
@@ -60,19 +48,23 @@ namespace FlightSimulator
         {
             if (this.mousePressed)
             {
-                this.current.X = e.GetPosition(sender as Canvas).X;
-                this.current.Y = e.GetPosition(sender as Canvas).Y;
-                Console.WriteLine("current X: {0}, current Y: {1}", this.current.X, this.current.Y);
+                //Console.WriteLine("current X: {0}, current Y: {1}", knobPosition.X, knobPosition.Y);
 
-                Canvas.SetLeft(Knob, this.current.X);
-                Canvas.SetTop(Knob, this.current.Y);
-                //double radius = getRadius(x, y, x1, y1);
+                this.current.X = e.GetPosition(this).X - this.start.X;
+                this.current.Y = e.GetPosition(this).Y - this.start.Y;
+
+                // limit the movement of the knob
+                if (Math.Sqrt(this.current.X * this.current.X + this.current.Y * this.current.Y) < (BlackBase.Width / 2))
+                {
+                    knobPosition.X = this.current.X;
+                    knobPosition.Y = this.current.Y;
+                }
+                else
+                {
+                    this.mousePressed = false;
+                    centerKnob_Completed(sender, e);
+                }
             }
-        }
-
-        private double getRadius(double x, double y, double x1, double y1)
-        {
-            return Math.Sqrt((x1 - x) * (x1 - x) + (y1 - y) * (y1 - y));
         }
     }
 }
